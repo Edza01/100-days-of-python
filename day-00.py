2172ad4d0fc4aa3f851ae6e8cf9ad16239331f7e
Edza <miclex82@tutanota.com>
2023-10-07 15:33:38 +0300

    day-00

--- day-00.py
+++ day-00.py
@@ -1,2 +1,17 @@
 # Printing a simple message
-print("Hello, world!")
+# print('Hello, world!\nWhat is your name?')
+
+# input('?:')
+
+# print('hello 2' + input('name 2?'))
+
+# print(len(input('name: ')))
+
+# name = input("name:")
+# print(name)
+
+
+# print('hello')
+# city = input('city: ')
+# pet = input('pet: ')
+# print(pet + city + '\n')
